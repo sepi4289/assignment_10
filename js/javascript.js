4a3879df6187c557c594154bc256ac15450b0e32
Pichardo <sharaypichardo@ufl.edu>
2018-03-23 16:24:36 -0400

    Initial commit

---
+++ js/javascript.js
@@ -0,0 +1,22 @@
+
+
+$(document).ready(function() {
+  $('.heroIcon').hide();
+
+  $('#batmanButton').click(
+    function(){
+    $('#batmanIcon').fadeToggle("slow");
+  });
+    $('#captainButton').click(
+        function(){
+        $('#captainIcon').fadeToggle("slow");
+          });
+    $('#spidermanButton').click(
+        function(){
+        $('#spidermanIcon').fadeToggle("slow");
+          });
+    $('#wonderButton').click(
+        function(){
+        $('#wonderIcon').fadeToggle("slow");
+          });
+});
